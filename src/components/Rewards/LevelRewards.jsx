2d009e355f388a8d34a1984a244ff8f1832a077c
n1ssaz <anisazace9@gmail.com>
2023-10-22 07:35:25 +0200

    rewards and tickets

--- src/components/Rewards/LevelRewards.jsx
+++ src/components/Rewards/LevelRewards.jsx
@@ -1,39 +1,43 @@
-import {Box, Typography} from "@mui/material";
+import {Box} from "@mui/material";
 import React from "react";
-import LockOpenIcon from '@mui/icons-material/LockOpen';
-import LockIcon from '@mui/icons-material/Lock';
-import TollIcon from '@mui/icons-material/Toll';
+import LoyaltyIcon from '@mui/icons-material/Loyalty';
 
-const LevelRewards = ()=>{
+const LevelRewards = ({flights})=>{
     const computeFillPercentage = (value, segmentValue) => {
         return (value / segmentValue) * 100;
     };
 
-    const ProgressStep = ({ value, icon, filledIcon, label }) => {
+    const ProgressStep = ({ value, label, total, index }) => {
         const maxSegmentValue = 20;  // Circle fills at 20 units
         const fillPercentage = computeFillPercentage(value, maxSegmentValue);
         const gradientStyle = {
-            background: `linear-gradient(90deg, #3880ff ${fillPercentage}%, #E5E7EB ${fillPercentage}%)`
+            background: `linear-gradient(90deg, ${total===30?'#9ac5db':total>=20?'#E5E4E2':total>=10?'#FFD700':total>=5?'#C0C0C0':'#CD7F32'} ${value===1?'100':'0'}%, #E5E7EB ${fillPercentage}%)`
         };
 
-        const isFullyFilled = value === maxSegmentValue;
+        const icons ={
+            0:<LoyaltyIcon htmlColor="white"/>,
+            2:<LoyaltyIcon htmlColor='white'/>,
+            4:<LoyaltyIcon htmlColor='white'/>,
+            6:<LoyaltyIcon htmlColor='white'/>,
+            8:<LoyaltyIcon htmlColor='white'/>
+        }
 
         return (
             <div className="flex flex-col items-center">
                 <div style={gradientStyle} className="w-16 h-16 rounded-full flex justify-center items-center">
-                    {isFullyFilled ? filledIcon : icon}
+                    {icons[index]}
                 </div>
                 <span className="mt-2">{label}</span>
             </div>
         );
     };
 
-    const ProgressLine = ({ value }) => {
-        const maxSegmentValue = 5;  // Line fills at 5 units
+    const ProgressLine = ({ value , total}) => {
+        const maxSegmentValue = 1;  // Line fills at 5 units
         const fillPercentage = computeFillPercentage(value, maxSegmentValue);
         const lineStyle = {
             width: `${fillPercentage}%`,
-            background: '#3880ff'
+            background:total===30?'#9ac5db':total>=20?'#E5E4E2':total>=10?'#FFD700':total>=5?'#C0C0C0':'#CD7F32'
         };
 
         return (
@@ -45,26 +49,25 @@ const LevelRewards = ()=>{
     };
 
     const ProgressTracker = ({ value }) => {
-        const segmentValues = [20, 5, 20, 5, 20, 5, 20];  // Circle-Line-Circle-Line sequence
+        const segmentValues = [1, 3, 1, 4, 1, 9, 1, 9, 1]; 
         const values = segmentValues.map((segment, index) => {
             let cumulativeValue = segmentValues.slice(0, index + 1).reduce((acc, curr) => acc + curr, 0);
             let remainder = value - cumulativeValue + segment;
             return Math.min(Math.max(remainder, 0), segment);
         });
-
         return (
             <div className="flex items-center">
                 {values.map((val, index, arr) => (
                     index % 2 === 0 ? (
                         <ProgressStep
                             key={index}
+                            total={value}
+                            index={index}
                             value={val}
-                            icon={<LockOpenIcon />}
-                            filledIcon={<LockIcon />}
-                            label={['Welcome', 'Silver', 'Gold', 'Diamond'][index / 2]}
+                            label={['Bronze', 'Silver', 'Gold',"Platinum", 'Diamond'][index / 2]}
                         />
                     ) : (
-                        <ProgressLine key={index} value={val} />
+                        <ProgressLine key={index} value={val} total={value} />
                     )
                 ))}
             </div>
@@ -73,27 +76,7 @@ const LevelRewards = ()=>{
 
     return(
         <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
-            <div className="flex justify-between">
-                <Typography variant="h6" gutterBottom>
-                    Flight Level
-                </Typography>
-                <div className="flex flex-col">
-                    <Typography variant="subtitle1" color="textSecondary" gutterBottom>
-                        Points to spend
-                    </Typography>
-                    <div className="flex justify-end">
-                        <TollIcon sx={{fontSize:30}}/>
-                        <Typography variant="h6" sx={{ml:1}} gutterBottom>
-                            40
-                        </Typography>
-                    </div>
-
-                </div>
-
-            </div>
-
-            <ProgressTracker value={40} />
-
+            <ProgressTracker value={flights} />
         </Box>
     )
 }
--- src/pages/Rewards.jsx
+++ src/pages/Rewards.jsx
@@ -1,12 +1,157 @@
+import { useEffect, useState } from "react";
 import LevelRewards from "../components/Rewards/LevelRewards.jsx";
+import { Box, Typography } from "@mui/material";
+import api from "../api/api.js";
+import { useSelector } from "react-redux";
 const Rewards = () => {
+    const access = useSelector((state) => state.auth.token);
+    const [currentData, setCurrentData]=useState({})
+    useEffect(()=>{
+        api.get('/discount_info/discount_info/' ,{
+            headers: {
+              Authorization: `Bearer ${access}`,
+              "Content-Type": "application/json",
+            },
+          }).then((res)=>setCurrentData(JSON.parse(res.data.data)))
+    },[])
+    const flightToReact = currentData?.all_tiers?.reduce((acc, {tier_name, flight_range})=>{
+        return acc = {...acc, [tier_name]: flight_range}
+    },{}) || {}
+
+    
     return (
-        <div className="grid grid-cols-2 gap-4 mx-[12%] my-10 ">
+        <div className="grid grid-cols-1 gap-4 mx-[12%] my-10 ">
             <div>
-                <LevelRewards />
+                <LevelRewards flights={currentData.flights_this_year} />
             </div>
             <div>
-
+                {!currentData.current_tier && (
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Welcome</Typography>
+                   <Typography variant="h5">You need {flightToReact['Bronze']?.[0]|| 0 } booking to be a Bronze Customer</Typography>
+                </Box>
+                )}
+                 {currentData.current_tier === 'Bronze'&&(
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Congratulations you now are <span style={{color:'#CD7F32'}}>Bronze Customer</span></Typography>
+                   <Typography variant="h5">You need {(flightToReact['Silver']?.[0] - currentData.flights_this_year)} booking to be  a <span style={{color:'#C0C0C0'}}>Silver Customer</span></Typography>
+                </Box>
+                )}
+                {currentData.current_tier === 'Silver'&&(
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Congratulations you now are <span style={{color:'#E5E4E2'}}>Silver Customer</span></Typography>
+                   <Typography variant="h5">You need {(flightToReact['Gold']?.[0] - currentData.flights_this_year)} booking to be  a <span style={{color:'#FFD700'}}>Gold Customer</span></Typography>
+                </Box>
+                )}
+                {currentData.current_tier === 'Gold'&&(
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Congratulations you now are <span style={{color:'#FFD700'}}>Gold Customer</span></Typography>
+                   <Typography variant="h5">You need {(flightToReact['Platinum']?.[0] - currentData.flights_this_year)} booking to be  a <span style={{color:'#E5E4E2'}}>Platinum Customer</span></Typography>
+                </Box>
+                )}
+                {currentData.current_tier === 'Platinum'&&(
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Congratulations you now are <span style={{color:'#E5E4E2'}}>Platinum Customer</span></Typography>
+                   <Typography variant="h5">You need {(flightToReact['Diamond']?.[0] - currentData.flights_this_year)} booking to be  a <span style={{color:'#9ac5db'}}>Diamond Customer</span></Typography>
+                </Box>
+                )}
+                 {currentData.current_tier === 'Diamond'&&(
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Congratulations you now are <span style={{color:'#9ac5db'}}>Diamond Customer</span></Typography>
+                </Box>
+                )}
+            </div>
+            {currentData.current_tier === 'Bronze' && (
+            <div>
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', border:'1px solid #CD7F32', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Benefits of beeing Bronze Customer</Typography>
+                   {Object.keys(currentData?.all_tiers?.find(({tier_name})=>tier_name==='Bronze')?.discounts || {}).map((e)=>{
+                    return <Typography color='#CD7F32' variant="h5">{e}: {currentData.all_tiers.find(({tier_name})=>tier_name==='Bronze').discounts[e]}% discount</Typography>
+                    })}
+                </Box>
+                </div>
+            )}
+            {currentData.current_tier === 'Silver' && (
+            <div>
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', border:'1px solid #E5E4E2', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Benefits of beeing Silver Customer</Typography>
+                   {Object.keys(currentData?.all_tiers?.find(({tier_name})=>tier_name==='Silver')?.discounts || {}).map((e)=>{
+                    return <Typography color='#E5E4E2' variant="h5">{e}: {currentData.all_tiers.find(({tier_name})=>tier_name==='Silver').discounts[e]}% discount</Typography>
+                    })}
+                </Box>
+                </div>
+            )}
+            {currentData.current_tier === 'Gold' && (
+            <div>
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', border:'1px solid #FFD700', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Benefits of beeing Gold Customer</Typography>
+                   {Object.keys(currentData?.all_tiers?.find(({tier_name})=>tier_name==='Gold')?.discounts || {}).map((e)=>{
+                    return <Typography color='#FFD700' variant="h5">{e}: {currentData.all_tiers.find(({tier_name})=>tier_name==='Gold').discounts[e]}% discount</Typography>
+                    })}
+                </Box>
+                </div>
+            )}
+            {currentData.current_tier === 'Platinum' && (
+            <div>
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', border:'1px solid #E5E4E2', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Benefits of beeing Platinum Customer</Typography>
+                    {Object.keys(currentData?.all_tiers?.find(({tier_name})=>tier_name==='Platinum')?.discounts || {}).map((e)=>{
+                    return <Typography color='#E5E4E2' variant="h5">{e}: {currentData.all_tiers.find(({tier_name})=>tier_name==='Platinum').discounts[e]}% discount</Typography>
+                    })}
+                </Box>
+                </div>
+            )}
+            {currentData.current_tier === 'Diamond' && (
+            <div>
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', border:'1px solid #9ac5db', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Benefits of beeing Diamond Customer</Typography>
+                    {Object.keys(currentData?.all_tiers?.find(({tier_name})=>tier_name==='Diamond')?.discounts || {}).map((e)=>{
+                    return <Typography color='#9ac5db' variant="h5">{e}: {currentData.all_tiers.find(({tier_name})=>tier_name==='Diamond').discounts[e]}% discount</Typography>
+                    })}
+                </Box>
+                </div>
+            )}
+            <div>
+                {!currentData.current_tier && (
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', border:'1px solid #CD7F32', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Benefits of beeing Bronze Customer</Typography>
+                   {Object.keys(currentData?.all_tiers?.find(({tier_name})=>tier_name==='Bronze')?.discounts || {}).map((e)=>{
+                    return <Typography color='#CD7F32' variant="h5">{e}: {currentData.all_tiers.find(({tier_name})=>tier_name==='Bronze').discounts[e]}% discount</Typography>
+                    })}
+                </Box>
+                )}
+                 {currentData.current_tier === 'Bronze' && (
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', border:'1px solid #C0C0C0', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Benefits of beeing Silver Customer</Typography>
+                   {Object.keys(currentData?.all_tiers?.find(({tier_name})=>tier_name==='Bronze')?.discounts || {}).map((e)=>{
+                    return <Typography color='#C0C0C0' variant="h5">{e}: {currentData.all_tiers.find(({tier_name})=>tier_name==='Silver').discounts[e]}% discount</Typography>
+                    })}
+                </Box>
+                )}
+                 {currentData.current_tier === 'Silver' && (
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', border:'1px solid #FFD700', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Benefits of beeing Gold Customer</Typography>
+                   {Object.keys(currentData?.all_tiers?.find(({tier_name})=>tier_name==='Silver')?.discounts || {}).map((e)=>{
+                    return <Typography color='#FFD700' variant="h5">{e}: {currentData.all_tiers.find(({tier_name})=>tier_name==='Gold').discounts[e]}% discount</Typography>
+                    })}
+                </Box>
+                )}
+                 {currentData.current_tier === 'Gold' && (
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', border:'1px solid #E5E4E2', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Benefits of beeing Platinum Customer</Typography>
+                   {Object.keys(currentData?.all_tiers?.find(({tier_name})=>tier_name==='Gold')?.discounts || {}).map((e)=>{
+                    return <Typography color='#E5E4E2' variant="h5">{e}: {currentData.all_tiers.find(({tier_name})=>tier_name==='Platinum').discounts[e]}% discount</Typography>
+                    })}
+                </Box>
+                )}
+                 {currentData.current_tier === 'Platinum' && (
+                <Box sx={{ padding: '2em', width:"100%", margin: 'auto', backgroundColor: '#FFF', border:'1px solid #9ac5db', borderRadius: '8px', boxShadow: '0px 0px 10px rgba(0,0,0,0.1)' }}>
+                   <Typography variant="h5">Benefits of beeing Diamond Customer</Typography>
+                   {Object.keys(currentData?.all_tiers?.find(({tier_name})=>tier_name==='Diamond')?.discounts || {}).map((e)=>{
+                    return <Typography color='#9ac5db' variant="h5">{e}: {currentData.all_tiers.find(({tier_name})=>tier_name==='Diamond').discounts[e]}% discount</Typography>
+                    })}
+                </Box>
+                )}
             </div>
         </div>
     );
