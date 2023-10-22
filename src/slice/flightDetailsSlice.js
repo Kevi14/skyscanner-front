0c3f41cba14021c7f63b9b3fe40758f844f333e5
Kebi <kevikostandini@gmail.com>
2023-10-22 05:35:26 +0200

    added disc price

--- src/slice/flightDetailsSlice.js
+++ src/slice/flightDetailsSlice.js
@@ -1,25 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    departure: '',
-    selectedDate: '',
-    arrival: '',
-    price: null,
+  departure: "",
+  selectedDate: "",
+  arrival: "",
+  price: null,
+  discountedPrice: "",
 };
 
 const flightDetailsSlice = createSlice({
-    name: 'flightDetails',
-    initialState,
-    reducers: {
-        setFlightDetails: (state, action) => {
-            state.departure = action.payload.departure;
-            state.selectedDate = action.payload.selectedDate;
-            state.arrival = action.payload.arrival;
-            state.price = action.payload.price;
-        },
-        clearFlightDetails: () => initialState,
+  name: "flightDetails",
+  initialState,
+  reducers: {
+    setFlightDetails: (state, action) => {
+      state.departure = action.payload.departure;
+      state.selectedDate = action.payload.selectedDate;
+      state.arrival = action.payload.arrival;
+      state.price = action.payload.price;
+      state.discountedPrice = action.payload.discountedPrice;
     },
+    clearFlightDetails: () => initialState,
+  },
 });
 
-export const { setFlightDetails, clearFlightDetails } = flightDetailsSlice.actions;
+export const { setFlightDetails, clearFlightDetails } =
+  flightDetailsSlice.actions;
 export default flightDetailsSlice.reducer;
