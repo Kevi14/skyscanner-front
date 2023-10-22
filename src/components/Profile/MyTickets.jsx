dda2f2f0e6b09f7848d265656c8eb41799d21d34
MLGEnio <97459297+MLGEnio@users.noreply.github.com>
2023-10-22 08:58:56 +0200

    ticket api change

--- src/components/Profile/MyTickets.jsx
+++ src/components/Profile/MyTickets.jsx
@@ -12,50 +12,51 @@ const MyTickets = ()=>{
     const access = useSelector((state) => state.auth.token);
     const [currentData, setCurrentData]=useState([])
     useEffect(()=>{
-        api.get('/bookings' ,{
+        api.get('/booking-classes/user_bookings' ,{
             headers: {
               Authorization: `Bearer ${access}`,
               "Content-Type": "application/json",
             },
           }).then((res)=>setCurrentData(res.data.data))
     },[])
-    const list = currentData.sort((a, b)=>new Date(b.tickets[0].issued_date) - new Date(a.tickets[0].issued_date))
+    console.log(currentData)
+    const list = currentData.sort((a, b)=>new Date(b?.tickets[0]?.issued_date) - new Date(a?.tickets[0]?.issued_date))
     return(
         <div className="rounded-md mb-7 mt- col-span-6 ml-3 ">
             <Typography style={{marginBottom:7}} variant='h4'>My Tickets</Typography>
             {list.map(({id, tickets})=>{
-            return(<Accordion key={id}>
+            return(<Accordion sx={{my:1}} key={id}>
                 <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
                     aria-controls="panel1a-content"
                     id="panel1a-header"
                 >
-                    <Typography>{tickets[0].booked_segments[0].origin.city}  {'to '}   {tickets[0].booked_segments[0].destination.city} | {tickets[0].booked_segments[0].departure_date}</Typography>
+                    <Typography>{tickets[0]?.booked_segments[0]?.origin?.city}  {'to '}   {tickets[0]?.booked_segments[0]?.destination?.city} | {tickets[0]?.booked_segments[0]?.departure_date}</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography fontWeight='bold'>
-                     Booking Number:{tickets[0].ticket_number}
+                     Booking Number:{tickets[0]?.ticket_number}
                     </Typography>
                     <Typography>
-                    <span style={{fontWeight:'bold'}}> Origin City:</span> {tickets[0].booked_segments[0].origin.city}
+                    <span style={{fontWeight:'bold'}}> Origin City:</span> {tickets[0]?.booked_segments[0]?.origin?.city}
                     </Typography>
                     <Typography>
-                    <span style={{fontWeight:'bold'}}> Origin Airport: </span>{tickets[0].booked_segments[0].origin.airport_name}
+                    <span style={{fontWeight:'bold'}}> Origin Airport: </span>{tickets[0]?.booked_segments[0]?.origin?.airport_name}
                     </Typography>
                     <Typography>
-                    <span style={{fontWeight:'bold'}}>Destionation City: </span>{tickets[0].booked_segments[0].destination.city}
+                    <span style={{fontWeight:'bold'}}>Destionation City: </span>{tickets[0]?.booked_segments[0]?.destination?.city}
                     </Typography>
                     <Typography>
-                    <span style={{fontWeight:'bold'}}> Destination Airport: </span>{tickets[0].booked_segments[0].destination.airport_name}
+                    <span style={{fontWeight:'bold'}}> Destination Airport: </span>{tickets[0]?.booked_segments[0]?.destination?.airport_name}
                     </Typography>
                     <Typography>
-                    <span style={{fontWeight:'bold'}}>Class:</span> {tickets[0].booked_segments[0].booking_class.type}
+                    <span style={{fontWeight:'bold'}}>Class:</span> {tickets[0]?.booked_segments[0]?.booking_class?.type}
                     </Typography>
                     <Typography>
-                    <span style={{fontWeight:'bold'}}>Departure Date:</span> {tickets[0].booked_segments[0].departure_date}
+                    <span style={{fontWeight:'bold'}}>Departure Date:</span> {tickets[0]?.booked_segments[0]?.departure_date}
                     </Typography>
                     <Typography>
-                    <span style={{fontWeight:'bold'}}>Traveller:</span>{tickets[0].traveller}
+                    <span style={{fontWeight:'bold'}}>Traveller:</span>{tickets[0]?.traveller}
                     </Typography>
                 </AccordionDetails>
             </Accordion>)
